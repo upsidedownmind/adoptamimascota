e1eaa72aa81d30ab800392e9aaefc179fdf56663
Francisco Affonso <francisco.affonso@gmail.com>
2012-02-12 19:44:56 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Adoptamimascota::Application.config.secret_token = 'cbf1afe0aaf8960854ca1e7db4809bfc43b8592dfccbbc1bd7f3022ae14e9ab22f19e2a75b6e104aa86980d78b4d8b489a0a01a907e94ad2d462f3ace8b1a2bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Adoptamimascota::Application.config.session_store :cookie_store, key: '_adoptamimascota_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Adoptamimascota::Application.config.session_store :active_record_store
